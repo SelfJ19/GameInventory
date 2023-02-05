7a8560b9f20f620ff3d24522e307eb7f4fe563a3
SelfJ19 <SELFJ1@ETSU.EDU>
2023-02-05 12:51:03 -0500

    finished the comments for the Program.cs

--- GameInventory/Program.cs
+++ GameInventory/Program.cs
@@ -8,20 +8,28 @@
 ///////////////////////////////////////////////////////////////////////////////
 namespace GameInventory
 {
-
+    /// <summary>
+    /// Creates the driver that runs the code for creating items and displaying them
+    /// </summary>
     public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main method that creates an instance of a backpack and item factory and then makes items to be added to the backpack, then displays the items inside of the backpack
+        /// </summary>
+        public static void Main()
         {
+            // creates a new instance of a BackPack
             BackPack backPack = new BackPack();
 
-            //use the ItemFactory to create some random items to add to the backpack
+            // creates a new instance of the ItemFactory
             ItemFactory itemFactory = new ItemFactory();
 
+            //use the ItemFactory to create some random items to add to the backpack inside a loop
             for (int i = 0; i < 10; i++)
             {
                 backPack.Add(itemFactory.MakeRandomItem());
             }
+
             //Display all items in the backpack
             Console.WriteLine(backPack.ListItems());
         }
